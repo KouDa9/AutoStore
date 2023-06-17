612b4aae14271d71cf99ca9862665162610d25a4
Даниил Коурин <kourin.daniil@gmail.com>
2023-06-17 19:09:53 +0300

    Добавьте файлы проекта.

---
+++ AutoStore/Pages/AddCarsPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutoStore.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddCarsPage.xaml
+    /// </summary>
+    public partial class AddCarsPage : Page
+    {
+        private Car car = new Car();
+        public AddCarsPage(Car selectedCar)
+        {
+            InitializeComponent();
+            if (selectedCar != null)
+                car = selectedCar;
+            DataContext = car;
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            if (string.IsNullOrEmpty(car.Name))
+                errors.AppendLine("Заполите строку1");
+            if (string.IsNullOrEmpty(car.Year.ToString()))
+                errors.AppendLine("Заполите строку2");
+            if (string.IsNullOrEmpty(car.Price.ToString()))
+                errors.AppendLine("Заполите строку3");
+            if(errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            if (car.Id == 0)
+                AutoStoreEntities.GetContext().Cars.Add(car);
+
+            try
+            {
+                AutoStoreEntities.GetContext().SaveChanges();
+                MessageBox.Show("Ифнормация сохранена");
+                Manager.MainFrame.GoBack();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
---
+++ AutoStore/Pages/AutoPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+
+namespace AutoStore.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AutoPage.xaml
+    /// </summary>
+    public partial class AutoPage : Page
+    {
+        public AutoPage()
+        {
+            InitializeComponent();
+        }
+
+        private void DGridCars_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void UpdateBtn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddCarsPage((sender as System.Windows.Controls.Button).DataContext as Car));
+        }
+
+        private void AddBtn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddCarsPage(null));
+        }
+
+        private void AutoIsVisible(object sender, System.Windows.DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                AutoStoreEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(c => c.Reload());
+                DGridCars.ItemsSource = AutoStoreEntities.GetContext().Cars.ToList();
+            }
+        }
+
+        private void DeleteBtn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var carsForRemove = DGridCars.SelectedItems.Cast<Car>().ToList();
+            if (System.Windows.MessageBox.Show("Вы точно хотите удалить?", "Внимание!", (MessageBoxButton)MessageBoxButtons.YesNo, (MessageBoxImage)MessageBoxIcon.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    AutoStoreEntities.GetContext().Cars.RemoveRange(carsForRemove);
+                    AutoStoreEntities.GetContext().SaveChanges();
+                    System.Windows.MessageBox.Show("Данные удалены");
+
+                    DGridCars.ItemsSource = AutoStoreEntities.GetContext().Cars.ToList();
+                }
+                catch (Exception ex)
+                {
+                    System.Windows.MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+    }
+}
